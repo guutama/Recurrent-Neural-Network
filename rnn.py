c450bcfb6c640d2154de69a888d5064ddb0aaa74
Gutama Ibrahim <giibrahimgu1@gmail.com>
2020-03-18 23:45:33 +0100

    did nothing

--- rnn.py
+++ rnn.py
@@ -1,5 +1,12 @@
 import numpy as np
-
+X=np.array([2,1]).reshape(2,1)
+W=np.array([2,2]).reshape(1,2)
 class MULGATE:
    def forward(self,X,W):
         return np.dot(X,W)
+
+
+
+if __name__=="__main__":
+    m=MULGATE(	)
+    print(m.forward(X,W).shape)
